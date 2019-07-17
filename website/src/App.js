ecfd2de08d9f0ad2f675228d1ec4fc7396e51695
Collin Van Dyck <collin@segment.com>
2019-07-17 14:44:07 -0400

    Use the hash router for the react app.
    This plays nicely with hosting.

--- website/src/App.js
+++ website/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import { Pane } from "evergreen-ui";
 import Nav from "./Nav";
 import Content from "./Content";
