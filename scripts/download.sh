6f1fbe52dbc363472c1e60001d986439a0bc4915
Taylor Brennan <taylor.brennan@segment.com>
2023-08-09 15:44:13 -0400

    add concurrency parameter

--- scripts/download.sh
+++ scripts/download.sh
@@ -3,11 +3,12 @@
 set -eo pipefail
 
 CTLSTORE_BOOTSTRAP_URL=$1
+CONCURRENCY=${2:-20}
 
 if [ ! -f /var/spool/ctlstore/ldb.db ]; then
   mkdir -p /var/spool/ctlstore
   cd /var/spool/ctlstore
-  s5cmd -r 0 --log debug cp $CTLSTORE_BOOTSTRAP_URL .
+  s5cmd -r 0 --log debug cp --concurrency $CONCURRENCY $CTLSTORE_BOOTSTRAP_URL .
 
   if [[ ${CTLSTORE_BOOTSTRAP_URL: -2} == gz ]]; then
     echo "Decompressing"
