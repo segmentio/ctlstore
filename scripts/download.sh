51c6b2e95ee2ac77be9bf93221f1c2b87e76d0cc
Taylor Brennan <taylor.brennan@segment.com>
2023-08-10 12:59:04 -0400

    Add metrics to the download script

--- scripts/download.sh
+++ scripts/download.sh
@@ -4,19 +4,36 @@ set -eo pipefail
 
 CTLSTORE_BOOTSTRAP_URL=$1
 CONCURRENCY=${2:-20}
+STATS_IP=$3
+STATS_PORT=${4:-8125}
 
+TAGS="downloaded:false"
+START=$(date +%s)
+END=$(date +%s)
 if [ ! -f /var/spool/ctlstore/ldb.db ]; then
+  # busybox does not support sub-second resolution
+  START=$(date +%s)
+
   mkdir -p /var/spool/ctlstore
   cd /var/spool/ctlstore
   s5cmd -r 0 --log debug cp --concurrency $CONCURRENCY $CTLSTORE_BOOTSTRAP_URL .
 
+  TAGS="downloaded:true"
   if [[ ${CTLSTORE_BOOTSTRAP_URL: -2} == gz ]]; then
     echo "Decompressing"
     pigz -d snapshot.db.gz
+    TAGS="$TAGS,compressed:true"
   fi
 
+  TAGS="$TAGS,concurrency:$CONCURRENCY"
+
   mv snapshot.db ldb.db
-  echo "ldb.db ready"
+  END=$(date +%s)
+  echo "ldb.db ready in $(($END - $START)) seconds"
 else
   echo "Snapshot already present"
+fi
+
+if [ ! -z "$STATS_IP" ]; then
+  echo -n "ctlstore.reflector.init_snapshot_download_time:$(($END - $START))|h|#$TAGS" | nc -u -w1 $NODE_IP $STATS_PORT
 fi
