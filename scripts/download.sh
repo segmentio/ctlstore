a3aa95a9269b634d49db9a19459e2e51aaa686cb
Taylor Brennan <taylor.brennan@segment.com>
2023-08-24 17:01:06 -0400

    only make copies from an ldb not in use

--- scripts/download.sh
+++ scripts/download.sh
@@ -6,7 +6,6 @@ CTLSTORE_BOOTSTRAP_URL=$1
 CONCURRENCY=${2:-20}
 NUM_LDB=${3:-1}
 
-TAGS="downloaded:false"
 START=$(date +%s)
 END=$(date +%s)
 
@@ -18,14 +17,20 @@ if [ ! -f /var/spool/ctlstore/ldb.db ]; then
   START=$(date +%s)
   s5cmd -r 0 --log debug cp --concurrency $CONCURRENCY $CTLSTORE_BOOTSTRAP_URL .
 
-  TAGS="downloaded:true"
   if [[ ${CTLSTORE_BOOTSTRAP_URL: -2} == gz ]]; then
     echo "Decompressing"
     pigz -d snapshot.db.gz
-    TAGS="$TAGS,compressed:true"
   fi
 
-  TAGS="$TAGS,concurrency:$CONCURRENCY"
+  i=2
+  while [ "$i" -le $NUM_LDB ]; do
+    if [ ! -f  ldb-$i.db ]; then
+      echo "creating copy ldb-$i.db"
+      cp snapshot.db ldb-$i.db
+    fi
+    i=$(( i + 1 ))
+  done
+
   mv snapshot.db ldb.db
   END=$(date +%s)
   echo "ldb.db ready in $(($END - $START)) seconds"
@@ -33,11 +38,27 @@ else
   echo "Snapshot already present"
 fi
 
+# on existing nodes, we may already have the ldb file.
+# We should download a new snapshot to avoid copying an in-use ldb.db file and risking a malformed db
 i=2
 while [ "$i" -le $NUM_LDB ]; do
+
+  # make sure it's not already downloaded
   if [ ! -f  ldb-$i.db ]; then
+
+    # download the snapshot if it's not present
+    if [ ! -f /var/spool/ctlstore/snapshot.db ]; then
+      echo "Downloading a new snapshot for ldb copies"
+      s5cmd -r 0 --log debug cp --concurrency $CONCURRENCY $CTLSTORE_BOOTSTRAP_URL .
+    fi
+
     echo "creating copy ldb-$i.db"
-    cp ldb.db ldb-$i.db
+    cp snapshot.db ldb-$i.db
   fi
   i=$(( i + 1 ))
 done
+
+if [ -f /var/spool/ctlstore/snapshot.db ]; then
+  echo "removing snapshot.db"
+  rm snapshot.db
+fi
