38065709f3c970f123fcc81b25eaadf85057f9fb
Hongyu Zhou <hongyu.zhou@segment.com>
2023-09-19 16:47:11 -0400

    make generate

--- pkg/supervisor/s3_uploader.go
+++ pkg/supervisor/s3_uploader.go
@@ -4,7 +4,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
 )
 
-//counterfeiter:generate -o fakes/s3_uploader.go . S3Uploader
+//counterfeiter:generate -o fakes/s3_uploader.go . S3Client
 type S3Client interface {
 	manager.UploadAPIClient
 }
