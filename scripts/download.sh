aaca5d0e8576bc52dd5feb7f83bccd8607413d8e
Hongyu Zhou <hongyu.zhou@segment.com>
2023-09-21 11:10:48 -0400

    WIP test

--- scripts/download.sh
+++ scripts/download.sh
@@ -7,6 +7,7 @@ CONCURRENCY=${2:-20}
 DOWNLOADED="false"
 COMPRESSED="false"
 METRICS="/var/spool/ctlstore/metrics.json"
+SHASUM=""
 
 START=$(date +%s)
 END=$(date +%s)
@@ -16,7 +17,21 @@ if [ ! -f /var/spool/ctlstore/ldb.db ]; then
 
   mkdir -p /var/spool/ctlstore
   cd /var/spool/ctlstore
+
+
+  PREFIX="$(echo $CTLSTORE_BOOTSTRAP_URL | grep :// | sed -e's,^\(.*://\).*,\1,g')"
+  URL="$(echo $CTLSTORE_BOOTSTRAP_URL | sed -e s,$PREFIX,,g)"
+  BUCKET="$(echo $URL | grep / | cut -d/ -f1)"
+  KEY="$(echo $URL | grep / | cut -d/ -f2)"
+
+  aws s3api head-object \
+    --bucket "${BUCKET}" \
+    --key "${KEY}"
+
   s5cmd -r 0 --log debug cp --concurrency $CONCURRENCY $CTLSTORE_BOOTSTRAP_URL .
+
+  SHASUM=$(shasum -a 256 $CTLSTORE_BOOTSTRAP_URL | cut -f1 -d\ | xxd -r -p | base64)
+  echo "Sha value of the downloaded file: $(($SHASUM))"
 
   DOWNLOADED="true"
   if [[ ${CTLSTORE_BOOTSTRAP_URL: -2} == gz ]]; then
@@ -29,6 +44,18 @@ if [ ! -f /var/spool/ctlstore/ldb.db ]; then
   END=$(date +%s)
   echo "ldb.db ready in $(($END - $START)) seconds"
 else
+
+  CTLSTORE_BOOTSTRAP_URL="s3://segment-ctlstore-snapshots-stage/snapshot.db"
+
+  PREFIX="$(echo $CTLSTORE_BOOTSTRAP_URL | grep :// | sed -e's,^\(.*://\).*,\1,g')"
+  URL="$(echo $CTLSTORE_BOOTSTRAP_URL | sed -e s,$PREFIX,,g)"
+  BUCKET="$(echo $URL | grep / | cut -d/ -f1)"
+  KEY="$(echo $URL | grep / | cut -d/ -f2)"
+
+  aws s3api head-object \
+    --bucket "${BUCKET}" \
+    --key "${KEY}"
+
   echo "Snapshot already present"
 fi
 
