6b11bd8ffc661b329491f752521c36cc4c42ca5c
Erik Weathers <erik.weathers@segment.com>
2024-01-11 23:02:08 -0800

    update comment

--- pkg/event/entry.go
+++ pkg/event/entry.go
@@ -3,7 +3,7 @@ package event
 // entry represents a single row in the changelog
 // e.g.
 //
-//	{"seq":1,"family":"fam","table":"foo","key":[{"name":"id","type":"int","value":1}]}
+//	{"seq":1,"family":"fam","type":"insert","table":"foo","key":[{"name":"id","type":"int","value":1}]}
 type entry struct {
 	Seq    int64  `json:"seq"`
 	Type   string `json:"type"`
